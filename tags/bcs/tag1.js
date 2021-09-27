778edb844b5c644668de46578aa1818277bbec20
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2021-09-26 19:22:05 -0700

    update

---
+++ tags/bcs/tag1.js
@@ -0,0 +1,3 @@
+function tag1(){
+    document.getElementById("tag1").innerHTML = '<h3>Updating content has never been easier!</h3> <p>Just update your tag script and save. So simple!</p>'
+}
---
+++ tags/bcs/tag2.js
@@ -0,0 +1,3 @@
+function tag2(){
+    document.getElementById("tag2").innerHTML = '<p style="padding-top: 15px;">Just to prove how useful and effective this resource is, all of the text elements of this website are powered by it!</p>'
+}
